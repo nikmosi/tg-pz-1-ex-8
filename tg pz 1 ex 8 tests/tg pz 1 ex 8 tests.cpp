feb9ec817e04394a377d33a97852b04c828e1e8d
Kirill Malcev <nikmosvik@gmail.com>
2021-04-04 14:31:30 +0700

    create TEST_CLASS(speed_tests)

--- tg pz 1 ex 8 tests/tg pz 1 ex 8 tests.cpp
+++ tg pz 1 ex 8 tests/tg pz 1 ex 8 tests.cpp
@@ -5,10 +5,23 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace tg_pz_1_ex_8_tests
 {
-	TEST_CLASS(basetest)
+	TEST_CLASS(fail_tests)
 	{
 	public:
 		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+
+		TEST_METHOD(TestMethod2)
+		{
+		}
+	};
+
+	TEST_CLASS(speed_tests)
+	{
+	public:
+
 		TEST_METHOD(TestMethod1)
 		{
 		}
