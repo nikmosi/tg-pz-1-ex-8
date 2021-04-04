aae421c38d4f1f5fff1510ca743bb530a61d551a
Kirill Malcev <nikmosvik@gmail.com>
2021-04-04 15:32:16 +0700

    Create examples

--- tg pz 1 ex 8/main.cpp
+++ tg pz 1 ex 8/main.cpp
@@ -2,7 +2,19 @@
 #include <iostream>
 #include <chrono>
 #include <vector>
+#include <queue>
 #include <ratio>
+#include <set>
+#include <map>
+
+/// <summary>
+/// // checks if there is a way 'from' and 'to' in a 'graph' using breadth-first search
+/// </summary>
+/// <param name="graph"> Graph representation </param>
+/// <param name="from"> start point </param>
+/// <param name="to"> end point </param>
+/// <returns> 1 if there is a path, otherwise 0 </returns>
+bool has_path_bfs(const std::vector<std::vector<int>> &graph, int from, int to);
 
 /// <summary>
 /// loads a graph from 'file_name'
@@ -20,22 +32,48 @@ double execute(const std::function<void()> &func);
 
 int main()
 {
+   auto *graphs = new std::vector<std::vector<int>>*[] // graph collection
+   {
+      input(*new std::stringstream(
+         "18\n0 1\n1 2\n2 3\n3 4\n4 5\n5 6\n6 7\n7 8\n8 9\n9 10\n10 11\n11 12\n12 13\n13 14\n14 7\n8 12\n10 14\n11 1\n5 7\n5 15\n15 13\n15 2\n16 4\n16 1\n16 13\n0 11\n")),
+      input(*new std::stringstream("7\n0 1\n1 2\n2 3\n3 4\n4 5\n5 6\n6 0\n")),
+      input(*new std::stringstream("13\n0 1\n1 2\n1 3\n1 4\n1 6\n2 6\n6 5\n5 4\n7 8\n8 9\n8 10\n8 11\n9 10\n10 11\n11 12\n")),
+      input(*new std::stringstream("16\n0 1\n0 2\n0 3\n1 4\n1 5\n2 6\n2 7\n3 8\n3 9\n4 10\n4 11\n4 12\n5 13\n5 14\n6 15\n")),
+   };
+
    
-
-   execute([]() {});
-
 
    return 0;
 }
 
+bool has_path_bfs(const std::vector<std::vector<int>> &graph, const int from, const int to)
+{
+   auto *q = new std::queue<int>();
+   q->push(from);
+   auto *gray = new std::set<int>();
+   while(!q->empty())
+   {
+      int v = q->front();
+      q->pop();
+      gray->insert(v);
+      for(int j : graph[v])
+      {
+         if(gray->count(j) == 0) q->push(j);
+         if(j == to) return true;
+      }
+   }
+
+   return false;
+}
+
 std::vector<std::vector<int>> *input(std::istream &input)
 {
-   if (input)
+   if(input)
    {
       int n, m;
       input >> n >> m;
       std::vector<std::vector<int>> *graph = new std::vector<std::vector<int>>(n);
-      for (int i = 0; i < m; ++i)
+      while(!input.eof())
       {
          int a, b;
          input >> a >> b;
