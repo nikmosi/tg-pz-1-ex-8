81f84d59442fae046b82849d34a849887c9e71f4
Kirill Malcev <nikmosvik@gmail.com>
2021-04-04 14:15:50 +0700

    Create unit tests

---
+++ tg pz 1 ex 8 tests/tg pz 1 ex 8 tests.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace tgpz1ex8tests
+{
+	TEST_CLASS(tgpz1ex8tests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		   
+		}
+	};
+}
