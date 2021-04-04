eaaf0a90fa7b77e4d244036f9f513958c1c55d48
Kirill Malcev <nikmosvik@gmail.com>
2021-04-04 14:23:11 +0700

    fix main

--- tg pz 1 ex 8/main.cpp
+++ tg pz 1 ex 8/main.cpp
@@ -7,12 +7,12 @@
 int main()
 {
    auto s = std::ifstream("input.txt");
-   auto *graph = input(s);
+   auto *graph = path::input(s);
 
    if(graph)
    {
       bool r = 0;
-      const double time = execute([graph, &r]() { r = has_path_dfs(*graph, 1, 4); });
+      const double time = path::execute([graph, &r]() { r = path::has_path_dfs(*graph, 1, 4); });
       std::cout << time << " ms" << std::endl << (r ? "route exists" : "route does not exist") << std::endl;
    }
 
