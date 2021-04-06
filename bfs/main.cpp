54e1cc7c3f94b1dc1da631e148ec2d7c61c050a0
Kirill Malcev <nikmosvik@gmail.com>
2021-04-06 10:48:16 +0700

    add input and output functions

--- bfs/main.cpp
+++ bfs/main.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <queue>
 #include <set>
+#include <istream>
 
 /// <summary>
 /// // checks if there is a way 'from' and 'to' in a 'graph' using breadth-first search
@@ -11,6 +12,19 @@
 /// <returns> 1 if there is a path, otherwise 0 </returns>
 bool has_path_bfs(const std::vector<std::vector<int>> &graph, int from, int to);
 
+/// <summary>
+/// creates a graph view from an input stream
+/// </summary>
+/// <param name="input"></param>
+/// <returns> graph representation </returns>
+std::vector<std::vector<int>> *input(std::istream &input);
+
+/// <summary>
+/// creates a graph view in an output stream
+/// </summary>
+/// <param name="output"></param>
+/// <param name="graph"> graph representation </param>
+void output(std::ostream &output, std::vector<std::vector<int>> &graph);
 
 int main(int argc, char *argv[])
 {
@@ -34,4 +48,42 @@ bool has_path_bfs(const std::vector<std::vector<int>> &graph, const int from, co
    }
 
    return false;
+}
+
+std::vector<std::vector<int>> *input(std::istream &input)
+{
+   if (input)
+   {
+      int n, m;
+      input >> n >> m;
+      std::vector<std::vector<int>> *graph = new std::vector<std::vector<int>>(n);
+      for (int i = 0; i < m; ++i)
+      {
+         int a, b;
+         input >> a >> b;
+         graph->at(a).push_back(b);
+         graph->at(b).push_back(a);
+      }
+      return graph;
+   }
+   return nullptr;
+}
+
+void output(std::ostream &output, const std::vector<std::vector<int>> *&graph)
+{
+   const auto n = graph->size();
+   auto m = 0;
+
+   for (const auto &i : graph)
+      m += i.size();
+   m /= 2;
+   output << n << " " << m << std::endl;
+
+   for (size_t i = 0; i < n; ++i)
+   {
+      const auto &e = graph->at(i);
+      const auto s = e.size();
+      for (size_t j = 0; j < s; ++j)
+         output << i << e.at(j) << std::endl;
+   }
 }
--- dfs/main.cpp
+++ dfs/main.cpp
@@ -1,5 +1,7 @@
+#include <istream>
 #include <vector>
 #include <set>
+
 
 /// <summary>
 /// checks if there is a way 'from' and 'to' in a 'graph' using Depth-first search
@@ -20,11 +22,23 @@ bool has_path_dfs(const std::vector<std::vector<int>> &graph, int from, int to);
 /// <returns> 1 if there is a path, otherwise 0 </returns>
 bool dfs(const std::vector<std::vector<int>> &graph, int from, int to, std::set<int> &gray);
 
+/// <summary>
+/// creates a graph view from an input stream
+/// </summary>
+/// <param name="input"></param>
+/// <returns> graph representation </returns>
+std::vector<std::vector<int>> *input(std::istream &input);
+
+/// <summary>
+/// creates a graph view in an output stream
+/// </summary>
+/// <param name="output"></param>
+/// <param name="graph"> graph representation </param>
+void output(std::ostream &output, std::vector<std::vector<int>> &graph);
 
 int main(int argc, char *argv[])
 {
 }
-
 
 bool has_path_dfs(const std::vector<std::vector<int>> &graph, const int from, const int to)
 {
@@ -43,3 +57,42 @@ bool dfs(const std::vector<std::vector<int>> &graph, const int from, const int t
    }
    return false;
 }
+
+std::vector<std::vector<int>> *input(std::istream &input)
+{
+   if(input)
+   {
+      int n, m;
+      input >> n >> m;
+      std::vector<std::vector<int>> *graph = new std::vector<std::vector<int>>(n);
+      for(int i = 0; i < m; ++i)
+      {
+         int a, b;
+         input >> a >> b;
+         graph->at(a).push_back(b);
+         graph->at(b).push_back(a);
+      }
+      return graph;
+   }
+   return nullptr;
+}
+
+void output(std::ostream &output, const std::vector<std::vector<int>> *&graph)
+{
+   const auto n = graph->size();
+   auto m = 0;
+
+   for (const auto &i : graph)
+      m += i.size();
+   m /= 2;
+   output << n << " " << m << std::endl;
+
+   for (size_t i = 0; i < n; ++i)
+   {
+      const auto &e = graph->at(i);
+      const auto s = e.size();
+      for (size_t j = 0; j < s; ++j)
+         output << i << e.at(j) << std::endl;
+   }
+}
+
