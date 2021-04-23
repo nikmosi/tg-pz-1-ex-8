8e7051ad12f6f2d70b0235626669ea30c51e6624
Kirill Mlacev n <nikmosvok@gmail.com>
2021-04-23 14:22:47 -0700

    fix output

--- useful functions/output.h
+++ useful functions/output.h
@@ -2,30 +2,34 @@
 
 #include <ostream>
 #include <vector>
+#include <set>
 
 namespace useful_func
 {
-   /// <summary>
-   /// creates a graph view in an output stream
-   /// </summary>
-   /// <param name="output"></param>
-   /// <param name="graph"> graph representation </param>
-   inline void output(std::ostream &output, std::vector<std::vector<int>> &graph)
-   {
-      const auto n = graph.size();
-      auto m = 0;
+	/// <summary>
+	/// creates a graph view in an output stream
+	/// </summary>
+	/// <param name="output"></param>
+	/// <param name="graph"> graph representation </param>
+	inline void output(std::ostream& output, std::vector<std::vector<int>>& graph)
+	{
+		const auto n = graph.size();
+		auto m = 0;
 
-      for (const auto &i : graph)
-         m += i.size();
-      m /= 2;
-      output << n << " " << m << std::endl;
+		for (const auto& i : graph)
+			m += i.size();
+		m /= 2;
+		output << n << " " << m << std::endl;
 
-      for (size_t i = 0; i < n; ++i)
-      {
-         const auto &e = graph.at(i);
-         const auto s = e.size();
-         for (size_t j = 0; j < s; ++j)
-            output << i << " " << e.at(j) << std::endl;
-      }
-   }
+		std::set<int>* marked = new std::set<int>();
+		for (size_t i = 0; i < n; ++i)
+		{
+			marked->insert(i);
+			const auto& e = graph.at(i);
+			const auto s = e.size();
+			for (size_t j = 0; j < s; ++j)
+				if (marked->count(e.at(j)) == 0)
+					output << i << " " << e.at(j) << std::endl;
+		}
+	}
 }
