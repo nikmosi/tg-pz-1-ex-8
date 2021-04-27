81317fe40fbce22dc140825a190f826eaaecfcb4
Kirill Malcev <nikmosvik@gmail.com>
2021-04-28 06:08:01 +0700

    changed '->' to '.'

--- bfs/main_bfs.cpp
+++ bfs/main_bfs.cpp
@@ -29,17 +29,17 @@ int main(int argc, char *argv[])
 
 bool has_path_bfs(const std::vector<std::vector<int>> &graph, const int from, const int to)
 {
-   auto *q = new std::queue<int>();
-   q->push(from);
+   std::queue<int> q;
+   q.push(from);
    auto *gray = new std::set<int>();
-   while(!q->empty())
+   while(!q.empty())
    {
-      int v = q->front();
-      q->pop();
+      int v = q.front();
+      q.pop();
       gray->insert(v);
       for(int j : graph[v])
       {
-         if(gray->count(j) == 0) q->push(j);
+         if(gray->count(j) == 0) q.push(j);
          if(j == to) return true;
       }
    }
